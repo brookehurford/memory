0eaf71669dce394be165f7963aeff5ec7aad0aca
Brooke Hurford and Taylor Pokoj <brookebhurford@gmail.com>
2016-03-14 16:27:01 -0700

    Tried our best

--- js/browser.js
+++ js/browser.js
@@ -1,7 +1,26 @@
+
+
 $(document).ready(function(){
+  var clickCount = 0;
   $("i").hide();
+
   $(".card").click(function(){
-    debugger;
-  $(this).children('i').toggle();
+
+    $(this).children('i').toggle();
+    $(this).children('i').toggleClass('clicked');
+    clickCount += 1;
+
+    console.log(clickCount);
+
+    if (clickCount === 2) {
+      if ($(".clicked").hasClass("fa-car")) {
+        console.log("has class fa-car");
+          $(".clicked").addClass("matched");
+      } else {
+        $("i").hide();
+      }
+      clickCount = 0;
+      $("i").removeClass("clicked");
+    }
   });
 });
---
+++ js/memory.js
@@ -0,0 +1,4 @@
+function CardFlipGame(card, match) {
+  this.card = card;
+  this.match = match;
+}
