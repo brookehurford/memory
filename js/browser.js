bbf993972872af33f7ec9a5ba7cb131cce7d47fa
Brooke Hurford and Taylor Pokoj <pokojt@gmail.com>
2016-03-14 16:44:37 -0700

    Get the game to work by replacing id with data type

--- js/browser.js
+++ js/browser.js
@@ -1,5 +1,4 @@
 
-
 $(document).ready(function(){
   var clickCount = 0;
   $("i").hide();
@@ -13,14 +12,42 @@ $(document).ready(function(){
     console.log(clickCount);
 
     if (clickCount === 2) {
-      if ($(".clicked").hasClass("fa-car")) {
-        console.log("has class fa-car");
+      if ($(".clicked").first().parent().attr("data-type") === $(".clicked").last().parent().attr("data-type")) {
           $(".clicked").addClass("matched");
       } else {
-        $("i").hide();
+        $(".clicked").hide();
       }
       clickCount = 0;
       $("i").removeClass("clicked");
     }
   });
 });
+
+
+
+
+//
+// $(document).ready(function(){
+//   var clickCount = 0;
+//   $("i").hide();
+//
+//   $(".card").click(function(){
+//
+//     $(this).children('i').toggle();
+//     $(this).children('i').toggleClass('clicked');
+//     clickCount += 1;
+//
+//     console.log(clickCount);
+//
+//     if (clickCount === 2) {
+//       if ($(".clicked").hasClass("fa-car")) {
+//         console.log("has class fa-car");
+//           $(".clicked").addClass("matched");
+//       } else {
+//         $(".clicked").hide();
+//       }
+//       clickCount = 0;
+//       $("i").removeClass("clicked");
+//     }
+//   });
+// });
