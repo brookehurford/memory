6f8b5bdc24380d440c95a9ba952ecde65afcdf11
Brooke Hurford and Taylor Pokoj <pokojt@gmail.com>
2016-03-14 13:36:01 -0700

    Add initial file setup

---
+++ js/cli.js
@@ -0,0 +1,15 @@
+// var JournalEntry = require('./journal.js').JournalEntry;
+// var prompt = require('prompt');
+//
+// prompt.get('entry', function(err, result){
+//   var body = result.entry;
+//   var newEntry = new JournalEntry("", body);
+//   console.log(newEntry.body);
+//   var count = newEntry.wordCount();
+//   console.log(count);
+//
+//
+//   var anotherEntry = new JournalEntry("this is a title", "this is a body");
+//   console.log(anotherEntry.body);
+//   console.log(anotherEntry.wordCount());
+// });
